357225ba49770adedb24d43dc0326a36a2171c1c
Chen, Chien-ting <chenjt30@gmail.com>
2016-04-09 06:17:36 +0800

    0.0.1 released

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#-*-coding:utf-8-*-
+from distutils.core import setup
+from findarray30code.__version__ import __version__
+
+setup(
+    name='FindArray30Code',
+    version=__version__,
+    author='Yoxem Chen',
+    url="https://github.com/Yoxem/findarray30code",
+    requires=['PyQt4'],
+    packages=['findarray30code',],
+    license='X11 License',
+    long_description=open('README').read(),
+    package_data={'findarray30code': ['tables/*' , 'LICENSE'],
+              },
+    scripts=['bin/findarray30code'],
+)
